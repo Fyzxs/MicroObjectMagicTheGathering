6c1f9a17c1d441298eb358700c08af4f858c7c9c
Fyzxs <Fyzxs@users.noreply.github.com>
2018-01-13 23:24:06 -0800

    Event bus exists!

---
+++ MicroObjectMagicTheGathering/EventBusTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MicroObjectMagicTheGathering
+{
+    [TestClass]
+    public class EventBusTests
+    {
+        [TestMethod]
+        public void Exists()
+        {
+            new EventBus();
+        }
+    }
+
+    public class EventBus { }
+}
